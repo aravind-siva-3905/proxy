f3728ed89c3de33822f1ce8421ed7149cd6ae3c5
Aravind S <aravind.siva@zohocorp.com>
2025-07-04 01:36:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 // Handle CORS preflight requests
-app.options('*', (req, res) => {
+app.options('/', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, mcp-protocol-version');
